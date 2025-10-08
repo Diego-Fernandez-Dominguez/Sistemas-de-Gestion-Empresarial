6511c64583627f60a2777f146efffda71ed64d48
Diego-Fernandez-Dominguez <diego.fernandez@iesnervion.es>
2025-10-08 14:41:17 +0200

    Pruebas chorras

--- Unidad6/01-HelloWorld/01-HelloWorld/obj/Debug/net9.0/01-HelloWorld.AssemblyInfo.cs
+++ Unidad6/01-HelloWorld/01-HelloWorld/obj/Debug/net9.0/01-HelloWorld.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01-HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+385b2cc64b9dc4c249e456c19709509a4fff6915")]
 [assembly: System.Reflection.AssemblyProductAttribute("01-HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01-HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
