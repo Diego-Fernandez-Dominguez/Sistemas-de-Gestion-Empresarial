5d95c1cdeddce26b78802d89014ffcbfaca5a900
Diego-Fernandez-Dominguez <diego.fernandez@iesnervion.es>
2025-10-21 21:41:01 +0200

    Arreglo Editar Persona

--- Unidad7/01-ViewBag/01-ViewBag/obj/Debug/net9.0/01-ViewBag.AssemblyInfo.cs
+++ Unidad7/01-ViewBag/01-ViewBag/obj/Debug/net9.0/01-ViewBag.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01-ViewBag")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41641cab542cc86e5d82cd3f84a99c61bfdbc1ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cde2910d2456fc58c9aca6aca7110030a186607")]
 [assembly: System.Reflection.AssemblyProductAttribute("01-ViewBag")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01-ViewBag")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
