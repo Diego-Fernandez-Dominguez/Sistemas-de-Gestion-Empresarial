03fc45ea57b112c43060b07932559832de394e64
Quick <quicksuperior6@gmail.com>
2025-11-23 18:31:03 +0100

    Fernando, temo por mi vida

---
+++ Unidad10/U10-Ej1/Domain/Interfaces/UseCases/IUseCase.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities;
+using System.Collections.Generic;
+
+namespace Domain.Interfaces
+{
+    public interface IUseCase
+    {
+        List<clsPersona> getListaPersonas();
+    }
+}
