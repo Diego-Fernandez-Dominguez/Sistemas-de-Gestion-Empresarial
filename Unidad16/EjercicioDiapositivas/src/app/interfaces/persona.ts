06f2071197403503927aae96375b46b5d374e49b
Diego-Fernandez-Dominguez <diego.fernandez@iesnervion.es>
2026-01-26 12:38:57 +0100

    merindo

---
+++ Unidad16/EjercicioDiapositivas/src/app/interfaces/persona.ts
@@ -0,0 +1,8 @@
+export interface Persona {
+    _id: number;
+    nombre: String;
+    apellidos: String;
+    fechaNacimiento: Date;
+    idDepartamento: number;
+    foto?: string;
+}
--- Unidad6/01-HelloWorld/01-HelloWorld/obj/Debug/net9.0/01-HelloWorld.AssemblyInfo.cs
+++ Unidad6/01-HelloWorld/01-HelloWorld/obj/Debug/net9.0/01-HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01-HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52fff646d5e8450e7ec8c63e643c30ab307845b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d608d6d95a75731419cf27756488ead7dd75cd37")]
 [assembly: System.Reflection.AssemblyProductAttribute("01-HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01-HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
